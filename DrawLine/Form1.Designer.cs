9a6da1b02cf427d5121241d627ffa8e84673e6f9
Deniss Dmitrijevs <dod3005@gmail.com>
2022-04-26 21:04:58 +0300

    Add project files.

---
+++ DrawLine/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace DrawLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(406, 29);
+            this.x1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(88, 23);
+            this.x1.TabIndex = 0;
+            this.x1.TextChanged += new System.EventHandler(this.NumericInput_KeyPress);
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(406, 68);
+            this.y1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(88, 23);
+            this.y1.TabIndex = 1;
+            this.y1.TextChanged += new System.EventHandler(this.NumericInput_KeyPress);
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(536, 68);
+            this.y2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(88, 23);
+            this.y2.TabIndex = 3;
+            this.y2.TextChanged += new System.EventHandler(this.NumericInput_KeyPress);
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(536, 29);
+            this.x2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(88, 23);
+            this.x2.TabIndex = 2;
+            this.x2.TextChanged += new System.EventHandler(this.NumericInput_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(366, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Y2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(508, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "X2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 116);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Draw line";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.y2);
+            this.Controls.Add(this.x2);
+            this.Controls.Add(this.y1);
+            this.Controls.Add(this.x1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ DrawLine/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLine
+{
+    public partial class Form1 : Form
+    {
+        int x1_val;
+        int x2_val;
+        int y1_val;
+        int y2_val;
+
+        bool firstCoords = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetCoordsAndDraw();
+        }
+
+        private void DrawPoint(Color color, int x, int y)
+        {
+            SolidBrush brush = new SolidBrush(color);
+
+            this.CreateGraphics().FillRectangle(brush, x, y, 1, 1);
+        }
+
+        private void DrawLine(int x1, int y1, int x2, int y2)
+        {
+            if (Math.Abs(y2 - y1) < Math.Abs(x2 - x1))
+            {
+                if (x1 > x2)
+                {
+                    PlotLineLow(x2, y2, x1, y1);
+                }
+                else
+                {
+                    PlotLineLow(x1, y1, x2, y2);
+                }
+            }
+            else
+            {
+                if (y1 > y2)
+                {
+                    PlotLineHigh(x2, y2, x1, y1);
+                }
+                else
+                {
+                    PlotLineHigh(x1, y1, x2, y2);
+                }
+            }
+        }
+
+        private void PlotLineLow(int x1, int y1, int x2, int y2)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int yi = 1;
+            if (dy < 0)
+            {
+                yi = -1;
+                dy = (-1) * dy;
+            }
+
+            int error = (2 * dy) - dx;
+            int y = y1;
+
+            for (int x = x1; x < x2; x++)
+            {
+                DrawPoint(Color.Red, x, y);
+                if (error > 0)
+                {
+                    y += yi;
+                    error += (2 * (dy - dx));
+                }
+                else
+                {
+                    error += 2 * dy;
+                }
+            }
+        }
+
+        private void PlotLineHigh(int x1, int y1, int x2, int y2)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int xi = 1;
+            if (dx < 0)
+            {
+                xi = -1;
+                dx = (-1) * dx;
+            }
+
+            int error = (2 * dx) - dy;
+            int x = x1;
+
+            for (int y = y1; y < y2; y++)
+            {
+                DrawPoint(Color.Blue, x, y);
+                if (error > 0)
+                {
+                    x += xi;
+                    error += (2 * (dx - dy));
+                }
+                else
+                {
+                    error += 2 * dx;
+                }
+            }
+        }
+
+        private void NumericInput_KeyPress(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int x = this.PointToClient(Cursor.Position).X;
+            int y = this.PointToClient(Cursor.Position).Y;
+            this.CreateGraphics().DrawRectangle(new Pen(Color.Black, 5), new Rectangle(x, y, 1, 1));
+            if (firstCoords)
+            {
+                x1.Text = x.ToString();
+                y1.Text = y.ToString();
+            }
+            else
+            {
+                x2.Text = x.ToString();
+                y2.Text = y.ToString();
+            }
+
+            firstCoords = !firstCoords;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char) Keys.Enter)
+            {
+                GetCoordsAndDraw();
+            }
+        }
+
+        private void GetCoordsAndDraw()
+        {
+            bool valid = true;
+
+            if (Int32.TryParse(x1.Text, out x1_val))
+            {
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(x2.Text, out x2_val))
+            {
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(y1.Text, out y1_val))
+            {
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (Int32.TryParse(y2.Text, out y2_val))
+            {
+            }
+            else
+            {
+                valid = false;
+            }
+
+            if (valid)
+            {
+                DrawLine(x1_val, y1_val, x2_val, y2_val);
+            }
+            else
+            {
+                MessageBox.Show("Coordinates must be integer numbers", "Invalid input");
+            }
+        }
+    }
+}
